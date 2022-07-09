7bd697a225525e5a99bc637c9f3ccbc1e1abff5c
Diana Morena <aneroma@yahoo.com>
2022-07-09 22:46:42 +0100

    Using react-query to manage the browser-based cache.

--- src/components/filterMoviesCard/index.js
+++ src/components/filterMoviesCard/index.js
@@ -1,4 +1,5 @@
-import React, {useState, useEffect}  from "react";
+import React from "react";  // useState/useEffect redundant
+//import React, {useState, useEffect}  from "react";
 import { getGenres } from "../../api/tmdb-api";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
@@ -12,6 +13,9 @@ import SearchIcon from "@material-ui/icons/Search";
 import FormControl from "@material-ui/core/FormControl";
 import Select from "@material-ui/core/Select";
 import img from '../../images/pexels-dziana-hasanbekava-5480827.jpg'
+import { useQuery } from "react-query";
+import Spinner from '../spinner'
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -29,23 +33,29 @@ const useStyles = makeStyles((theme) => ({
 
 export default function FilterMoviesCard(props) {
     const classes = useStyles();
-    const [genres, setGenres] = useState([{ id: '0', name: "All" }])
+    const { data, error, isLoading, isError } = useQuery("genres", getGenres);
   
-    useEffect(() => {
-        getGenres().then((allGenres) => {
-          setGenres([genres[0], ...allGenres]);
-        });
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-      }, [])
-    const handleChange = (e, type, value) => {
-        e.preventDefault()
-        props.onUserInput(type, value)   // NEW
-      }
-    const handleTextChange = e => {
-      handleChange(e, "name", e.target.value)
+    if (isLoading) {
+      return <Spinner />;
     }
-    const handleGenreChange = e => {
-      handleChange(e, "genre", e.target.value)
+  
+    if (isError) {
+      return <h1>{error.message}</h1>;
+    }
+    const genres = data.genres;
+    genres.unshift({ id: "0", name: "All" });
+  
+    const handleChange = (e, type, value) => {
+      e.preventDefault();
+      props.onUserInput(type, value); // NEW
+    };
+  
+    const handleTextChange = (e, props) => {
+      handleChange(e, "name", e.target.value);
+    };
+  
+    const handleGenreChange = (e) => {
+      handleChange(e, "genre", e.target.value);
     };
 
   return (
